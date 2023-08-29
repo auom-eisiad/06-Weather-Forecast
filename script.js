a0efc3c1db664cbf909541a71a59adf812dba63b
auom.eisiad <daisiemouaa@gmail.com>
2023-08-28 22:54:01 -0400

    build on script

--- script.js
+++ script.js
@@ -2,26 +2,43 @@
 var searchForm = $("#search-form");
 var searchInput = $("#search-input");
 var searchHistory = $("#search-history");
-var forecast = $("#weather-forecast");
+var forecast = $("#weather-list");
 var submit = $("#submit-button");
 
  // Hold the API key and store city name from user's input
- var apiKey = "26969c8a80538530cc05b672bf826665";
- var city;
- 
- function getAPI() {
+var apiKey = "26969c8a80538530cc05b672bf826665";
+var city;
+
+// Get user's city input
+var citySubmission = function(event) {
+  event.preventDefault();
+
+  var cityName = searchInput.value.trim();
+
+  if (cityName) {
+    getWeatherAPI(cityName);
+
+    forecast.textContent = '';
+    searchInput.value = '';
+  }
+  else {
+    alert('Please enter a city name!');
+  }
+};
+
+// Get weather information from third party
+function getWeatherAPI() {
   
+  // Link the city and apikey to the url
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
   
-  // fetch(queryURL).then(function(response) {
-  //   if(response.ok) {
-  //     response
-  //   }
-  // });
+  // Send an API request to the third party to get weather forecast
+  fetch(queryURL).then(function(response) {
+    if(response.ok) {
+      response
+    }
+  });
   
-}
+};
 
-submit.on('click', function(event) {
-  event.preventDefault();
-  console.log("click");
-})
+searchForm.addEventListener('submit', citySubmission);
