7de0ebecdf51e1707821dae9d30b505bffb1e095
auom.eisiad <daisiemouaa@gmail.com>
2023-08-29 22:16:35 -0400

    added required weather data needed for html and fixing script

--- script.js
+++ script.js
@@ -4,41 +4,38 @@ var searchInput = $("#search-input");
 var searchHistory = $("#search-history");
 var forecast = $("#weather-list");
 var submit = $("#submit-button");
+var temp = $(".temp");
+var date = $(".date");
 
  // Hold the API key and store city name from user's input
 var apiKey = "26969c8a80538530cc05b672bf826665";
 var city;
 
 // Get user's city input
-var citySubmission = function(event) {
+submit.on("click", function(event) {
   event.preventDefault();
 
-  var cityName = searchInput.value.trim();
+  var city = searchInput.value;
 
-  if (cityName) {
-    getWeatherAPI(cityName);
+  if (city) {
+    // getWeatherAPI();
 
     forecast.textContent = '';
     searchInput.value = '';
   }
   else {
-    alert('Please enter a city name!');
+    alert('Please enter a valid city name!');
   }
-};
+});
 
-// Get weather information from third party
+function searchHistory(city) {
+  var 
+}
+
 function getWeatherAPI() {
-  
   // Link the city and apikey to the url
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
   
-  // Send an API request to the third party to get weather forecast
-  fetch(queryURL).then(function(response) {
-    if(response.ok) {
-      response
-    }
-  });
-  
-};
-
-searchForm.addEventListener('submit', citySubmission);
+  // // Send an API request to the third party to get weather forecast
+  // fetch(queryURL).then(response => response.json())
+}
