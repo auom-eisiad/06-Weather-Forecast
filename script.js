0983831715ede0f3ac44199641f110c608c9dce2
auom.eisiad <daisiemouaa@gmail.com>
2023-11-21 23:13:59 -0500

    cleaned and fixed some codes

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
  // Global variables
-var searchForm = $("#search-form");
 var searchInput = $("#search-input");
 var searchHistory = $("#search-history");
-var forecast = $("#weather-list");
 var submit = $("#submit-button");
-var temp = $(".temp");
-var date = $(".date");
 
  // Hold the API key and store city name from user's input
 var apiKey = "26969c8a80538530cc05b672bf826665";
 var city;
 
-// Get user's city input
 submit.on("click", function(event) {
   event.preventDefault();
 
-  var city = searchInput.value;
+  city = searchInput.val().trim();
 
-  if (city) {
-    // getWeatherAPI();
-
-    forecast.textContent = '';
-    searchInput.value = '';
-  }
-  else {
-    alert('Please enter a valid city name!');
-  }
+  getWeatherAPI();
 });
-
-function searchHistory(city) {
-  var 
-}
 
 function getWeatherAPI() {
   // Link the city and apikey to the url
   var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+  // console.log(queryURL);
   
-  // // Send an API request to the third party to get weather forecast
-  // fetch(queryURL).then(response => response.json())
+  // Send an API request to the third party to get city longitude and latitude
+  fetch(queryURL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function (data) {
+    // console.log(data);
+    const cityData = {
+      city: city,
+      lat: data.coord.lat,
+      lon: data.coord.lon
+    }
+    
+    // console.log(cityData);
+    return cityData;
+  });
 }
