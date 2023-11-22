4bfddf1c92ac1edf0d023dce42ea67c7486636f1
auom.eisiad <daisiemouaa@gmail.com>
2023-11-22 00:02:54 -0500

    got the city name and date displayed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var searchInput = $("#search-input");
 var searchHistory = $("#search-history");
 var submit = $("#submit-button");
 
- // Hold the API key and store city name from user's input
+// Hold the API key and store city name from user's input
 var apiKey = "26969c8a80538530cc05b672bf826665";
 var city;
 
@@ -53,5 +53,8 @@ function getWeatherAPI(data) {
   })
   .then(function(data) {
     // console.log(data);
-  })
+    $(".weatherToday").removeClass("d-none");
+    $(".cityName").text(city);
+    $(".cityName").append(" (" + dayjs().format('YYYY-MM-DD') + ")");
+  });
 }
